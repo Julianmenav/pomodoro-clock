54dd62550a6964ba58ba291ae586ebf07b8e8b40
Julianmenav <julianmenav2@gmail.com>
2022-08-18 20:09:32 +0200

    update container colors

--- src/style.js
+++ src/style.js
@@ -1,4 +1,4 @@
-const containerColors = [ "#abe963", "#b5cf22", "#0077ff", "#ff7df9", "#58f4ff", "#ffef62", "#ff7474"]
+const containerColors = [ "#abe963", "#b5cf22", "#0077ff", "#58f4ff", "#ffef62", "#ff7474"]
 export const containerStyle = {
   backgroundColor: containerColors[Math.floor(Math.random() * containerColors.length)]
 }
