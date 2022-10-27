e240812e5b337833b4f1301f61231fec2b260e9e
Julianmenav <julianmenav2@gmail.com>
2022-10-27 21:29:48 +0200

    Migrate from Webpack to vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      Hello Vite
+    </div>
+  )
+}
+
+export default App
--- src/style.js
+++
@@ -1,4 +0,0 @@
-const containerColors = [ "#abe963", "#b5cf22", "#0077ff", "#58f4ff", "#ffef62", "#ff7474"]
-export const containerStyle = {
-  backgroundColor: containerColors[Math.floor(Math.random() * containerColors.length)]
-}
